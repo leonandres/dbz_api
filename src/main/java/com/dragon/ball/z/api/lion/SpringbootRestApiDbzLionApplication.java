140d77b3864c6ae1563088477bcdeb25c491f4ec
Andrés León <andresleonb90@gmail.com>
2024-10-04 22:55:28 -0300

    Primer commit

---
+++ src/main/java/com/dragon/ball/z/api/lion/SpringbootRestApiDbzLionApplication.java
@@ -0,0 +1,13 @@
+package com.dragon.ball.z.api.lion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestApiDbzLionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestApiDbzLionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dragon/ball/z/api/lion/SpringbootRestApiDbzLionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dragon.ball.z.api.lion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestApiDbzLionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
