3bb17a333de4543d172c7610d7ef60fc8cf42366
Andrés León <andresleonb90@gmail.com>
2024-10-13 14:57:11 -0300

    Se agrega PersonajeResponse y se modifica controller con la respuesta de la api

--- src/main/java/com/dragon/ball/z/api/lion/DbzController.java
+++ src/main/java/com/dragon/ball/z/api/lion/DbzController.java
@@ -7,7 +7,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.awt.*;
 import java.net.URI;
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/personajes")
@@ -18,5 +21,11 @@ public class DbzController {
     @GetMapping("/{id}")
     public Personaje getPersonajePorId(@PathVariable int id){
         return restTemplate.getForObject(DBZ_URL + id, Personaje.class);
+    }
+
+    @GetMapping
+    public List<Personaje> getTodosLosPersonajes(){
+        PersonajeResponse response = restTemplate.getForObject(DBZ_URL, PersonajeResponse.class);
+        return Arrays.asList(response.getItems());
     }
 }
---
+++ src/main/java/com/dragon/ball/z/api/lion/PersonajeResponse.java
@@ -0,0 +1,14 @@
+package com.dragon.ball.z.api.lion;
+
+public class PersonajeResponse {
+
+    private Personaje[] items;
+
+    public Personaje[] getItems() {
+        return items;
+    }
+
+    public void setItems(Personaje[] items) {
+        this.items = items;
+    }
+}
